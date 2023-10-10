cfb24af951b73d24755050180a7ffd1524a8e171
MIke Ulm <maulmbuckeye@gmail.com>
2023-10-10 19:29:37 -0400

    Complete book up to "There’s a quicker way to do these 3 lines with .objects.create()"

--- functional_tests.py
+++ functional_tests.py
@@ -28,26 +28,12 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element(By.TAG_NAME, "h1").text
         self.assertIn("To-Do", header_text)
 
-        # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
-        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
-
-        # She types "Buy peacock feathers" into a text box
-        # (Edith's hobby is tying fly-fishing lures)
-        inputbox.send_keys("Buy peacock feathers")
-
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.user_enters_an_item("Buy peacock feathers")
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
-        inputbox.send_keys("Use peacock feathers to make a fly")
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.user_enters_an_item("Use peacock feathers to make a fly")
 
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table("1: Buy peacock feathers")
@@ -59,6 +45,18 @@ class NewVisitorTest(unittest.TestCase):
         self.fail("Finish the test!")
 
         # The page updates again, and now shows both items on her list
+
+    def user_enters_an_item(self, user_input):
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+        # She types "Buy peacock feathers" into a text box
+        # (Edith's hobby is tying fly-fishing lures)
+        inputbox.send_keys(user_input)
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
 
 if __name__ == '__main__':
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from lists.models import Item
 
 
 def home_page(request):
+    if request.method == 'POST':
+        item = Item()
+        item.text = request.POST.get("item_text", "")
+        item.save()
+        return redirect("/")
+
     return render(
         request,
         "home.html",
