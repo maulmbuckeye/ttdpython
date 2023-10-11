5ba0cae8aa2fd4166ac793af30c1d1619548eebe
MIke Ulm <maulmbuckeye@gmail.com>
2023-10-10 20:08:12 -0400

    adds numbering to items on web page

--- lists/tests.py
+++ lists/tests.py
@@ -15,11 +15,21 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "A new list item")
 
+    def test_redirects_after_POST(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+
         self.assertRedirects(response, "/")
 
     def test_only_saves_items_when_necessary(self):
         self.client.get("/")
         self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+        response = self.client.get("/")
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
 
 
 class ItemModelTest(TestCase):
